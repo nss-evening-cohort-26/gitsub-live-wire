fed2af6ff0c1b6a4df82971597a230bd1b7f55c2
Ross Morgan <ross.morgan933@gmail.com>
2024-02-07 18:10:10 -0600

    Create complex data structure for repos

---
+++ data/complexData.js
@@ -0,0 +1,63 @@
+const repoExamples = [
+
+  {
+    name: "Calculator",
+    description: "A calculator made using html, js, and css.",
+    tags: [
+      {
+        tName: "JavaScript"
+      },
+      {
+        tName: "CSS"
+      },
+      {
+        tName: "HTML"
+      }
+    ],
+  },
+  {
+    name: "Pet Adoption",
+    description: "An app to render cards on the DOM and filter the cards specific to each pet. Able to add and delete pets.",
+    tags: [
+      {
+        tName: "Javascript"
+      },
+      {
+        tName: "Rendering"
+      },
+      {
+        tName: "DOM"
+      }
+    ],
+  },
+  {
+    name: "Link in Bio",
+    description: "Create a link in bio page using html and css. The app has buttons to explore someone's profile.",
+    tags: [
+      {
+        tName: "Styling"
+      },
+      {
+        tName: "Buttons"
+      },
+      {
+        tName: "CSS"
+      }
+    ],
+  },
+  {
+    name: "Sorting Hat Assessment",
+    description: "First assessment on our own. Creating an app to sort students into specific houses. Create functions to add and expel students. The expel students are rendered to another column once they are expelled",
+    tags: [
+      {
+        tName: "Javascript"
+      },
+      {
+        tName: "Harry Potter"
+      },
+      {
+        tName: "Assessment"
+      }
+    ],
+  }
+]
