302bd37f6d7006e1278653f11ea8de179291e6b4
CHRISTOPHER COLE <CHRISTOPHERCOLE.SFG@GMAIL.COM>
2024-02-07 20:59:53 -0600

    added project loop and it appears on DOM, still need to edit it

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { renderToDom } from "./utils/renderToDom";
+import { renderToDom } from "./utils/renderToDom.js";
 
 const sideBarOnDom = ()=>{
   let domString = ``;
@@ -11,3 +11,35 @@ const startApp = () =>{
 }
 
 startApp();
+
+const projects = [
+  {
+    id: 1,
+    name: "Dusty",
+    color: "Green",
+    specialSkill: "Gives sincere apologies.",
+    type: "cat",
+    imageUrl:
+      "https://static.displate.com/857x1200/displate/2023-03-15/dadeeb74dcc7ed99f2da757226d69818_5670a0e43cfe81ada198f1864a784534.jpg",
+  }
+];
+
+
+const pinnedProjectsOnDom = (array) => {
+  let projectsDomString = "";
+  array.map((project) => {
+    projectsDomString += `<div class="card" style="width: 18rem;">
+    <h5 class="card-title">${project.name}</h5>
+    <img src="${project.imageUrl}" 
+    class="card-img-top" alt="...">
+    <div class="card-body">
+    <p class="card-text">Color: ${project.color}</p>
+    <p class="card-text">Special Skill: ${project.specialSkill}</p>
+    <p class="card-text">${project.type}</p>
+  </div>
+</div>`;
+});
+renderToDom("#appProjects", projectsDomString);
+};
+
+pinnedProjectsOnDom(projects);
